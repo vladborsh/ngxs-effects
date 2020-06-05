3a13cf1e46cb13c03a84262abf5dad8342f316e0
vlad.borsh <vladborsh95s322@gmail.com>
2020-06-05 10:07:58 +0300

    fix(public api): effect catch error export

--- projects/ngxs-effect/src/public-api.ts
+++ projects/ngxs-effect/src/public-api.ts
@@ -4,6 +4,7 @@
 export { Effect } from './lib/effect.decorator';
 export { EffectsStart } from './lib/effects-start.decorator';
 export { EffectsTerminate } from './lib/effects-terminate.decorator';
+export { EffectsCatchError } from './lib/effect-catch-error.decorator';
 export { NgxsEffectsModule } from './lib/effects.module';
 export { EFFECTS_ERROR_HANDLER } from './lib/config/constans';
 export { EffectErrorHandlerInterface } from './lib/interfaces/effect-error-handler.interface';
